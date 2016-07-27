9ddcb189e79a45f2aa7aa4a756a6fb81de2f2756
Ziyan <jaraidaniel@gmail.com>
2016-07-27 02:43:44 +0200

    [TWRP][WIP] tuna: twrp: add support for hardware encryption
    - use custombootimg.mk to place needed modules and config
      inside our recovery ramdisk
    
    - we need to use static smc_pa_ctrl and tf_daemon
      REVIEW: we can move this to hardware/ti/omap4, placed here
      for testing only
    
    Change-Id: I1d272db85d8d52e2bda76dbdc13a4099197dee78

--- device-omni.mk
+++ device-omni.mk
@@ -4,4 +4,11 @@ DEVICE_PACKAGE_OVERLAYS += $(DEVICE_FOLDER)/overlay-custom
 
 # TWRP Fstab
 PRODUCT_COPY_FILES += \
-	$(DEVICE_FOLDER)/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab
+	$(DEVICE_FOLDER)/recovery/root/init.recovery.tuna.rc:recovery/root/init.recovery.tuna.rc \
+	$(DEVICE_FOLDER)/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab \
+	$(DEVICE_FOLDER)/recovery/root/vendor/etc/smc_normal_world_android_cfg.ini:recovery/root/vendor/etc/smc_normal_world_android_cfg.ini \
+	$(DEVICE_FOLDER)/recovery/root/vendor/firmware/smc_pa_wvdrm.ift:recovery/root/vendor/firmware/smc_pa_wvdrm.ift
+
+# dummy compiling Android 7.1 TWRP inside Omni6 source
+PRODUCT_COPY_FILES += \
+	$(DEVICE_FOLDER)/recovery/root/res/dummy:recovery/root/res/dummy
